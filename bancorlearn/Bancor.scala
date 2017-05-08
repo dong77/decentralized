19d598c2ce6a6942aaca55838970638cba410ed9
wangdong <wangdong@zhongan.io>
2017-05-08 16:47:42 +0800

    v2

--- bancorlearn/Bancor.scala
+++ bancorlearn/Bancor.scala
@@ -2,59 +2,95 @@
 
 package bancor
 
-case class Market(
-  initialSupply: Long,
-  initialReserve: Long,
-  reserveRatioOpt: Option[Double] = None) {
+class TockerChangerManager {
+  var tokenMap = Map.empty[String, BancorToken]
+  var totalTokens = 0L
 
-  var supply = initialSupply
-  var reserve = initialReserve
-  val reserveRatio = reserveRatioOpt.getOrElse(reserve * 1.0 / supply)
-  def price = reserve / (supply * reserveRatio)
+  def addBancorToken(token: BancorToken) = {
+    assert(!tokenMap.contains(token.id))
+    totalTokens += token.supply
+    token.crr = token.supply * 1.0 / totalTokens
 
-  def sell(amountOfReserveToken: Long) = {
-    assert(amountOfReserveToken > 0)
-    val smartToken = reserveToken2SmartToken(amountOfReserveToken)
-    if (smartToken > 0) {
-      supply += smartToken
-      reserve += amountOfReserveToken
-    }
-    println(this)
-    smartToken
+    tokenMap.foreach { case (_, t) => t.crr *= (1 - token.crr) }
+    tokenMap += token.id -> token
   }
 
-  def buy(amountOfSmartToken: Long) = {
-    assert(amountOfSmartToken > 0)
-    val reserveToken = smartTokenToReserveToken(amountOfSmartToken)
-    if (reserveToken > 0) {
-      supply -= amountOfSmartToken
-      reserve -= reserveToken
-    }
-    println(this)
-    reserveToken
-  }
+  def getTokenChanger(srcId: String, destId: String) = new TokenChanger(tokenMap(srcId), tokenMap(destId))
 
-  def smartTokenToReserveToken(amount: Long) = ((Math.pow(1 + amount.toDouble / reserve, reserveRatio) - 1) * supply).toLong
-  def reserveToken2SmartToken(amount: Long) = ((Math.pow(1 + amount.toDouble / supply, 1 / reserveRatio) - 1) * reserve).toLong
-
-  override def toString() = s"supply $supply, reserve: $reserve, price: $price}}"
+  override def toString() = s"totalTokens: $totalTokens,\n${tokenMap.values.mkString("\n")}"
 }
 
-case class LMSR(implicit val b: Double) {
+class TokenChanger(src: BancorToken, dest: BancorToken) {
+  def sell(amount: Long) = {
+    val tokenAmount = src.buySmartToken(amount)
+    val destAmount = dest.sellSmartToken(tokenAmount)
+    destAmount
+  }
 
-  def cost(q1: Double, q2: Double)(implicit b: Double) = {
-    b * Math.log(Math.pow(Math.E, q1 / b) + Math.pow(Math.E, q2 / b))
+  def quoteSellPrice(amount: Long): Option[Double] = {
+    val tokenAmount = src.buySmartToken(amount, false)
+    println("tokenAmount: " + tokenAmount)
+    val destAmount = dest.sellSmartToken(tokenAmount, false)
+    println("destAmount: " + destAmount)
+    if (tokenAmount == 0 || destAmount == 0) None
+    else Some(destAmount * 1.0 / amount)
   }
 }
+
+case class BancorToken(
+  val id: String,
+  var supply: Long,
+  var reserve: Long,
+  var crr: Double = 1.0) {
+  def price = reserve / (supply * crr)
+
+  // Buy smart token with reserve token
+  def buySmartToken(reserveAmount: Long, perform: Boolean = true): Long = {
+    // assert(reserveAmount >= 0) // for now
+    val tokenAmount = r2s(reserveAmount)
+    if (tokenAmount == 0 || reserveAmount == 0) 0
+    else {
+      if (perform) {
+        supply += tokenAmount
+        reserve += reserveAmount
+      }
+      tokenAmount
+    }
+  }
+
+  def sellSmartToken(tokenAmount: Long, perform: Boolean = true): Long = {
+    // assert(tokenAmount >= 0) // for now
+    val reserveAmount = s2r(tokenAmount)
+    if (tokenAmount == 0 || reserveAmount == 0) 0
+    else {
+      if (perform) {
+        supply -= tokenAmount
+        reserve -= reserveAmount
+      }
+      reserveAmount
+    }
+  }
+
+  private def s2r(amount: Long) = ((Math.pow(1 + amount.toDouble / supply, 1 / crr) - 1) * reserve).toLong
+  private def r2s(amount: Long) = ((Math.pow(1 + amount.toDouble / reserve, crr) - 1) * supply).toLong
+
+  override def toString() = s"$id: $reserve, supply: $supply, crr: $crr, price: $price}}"
+}
+
+// case class LMSR(implicit val b: Double) {
+
+//   def cost(q1: Double, q2: Double)(implicit b: Double) = {
+//     b * Math.log(Math.pow(Math.E, q1 / b) + Math.pow(Math.E, q2 / b))
+//   }
+// }
 
 object Main extends App {
-  var market = Market(100000000, 10000, Some(0.01))
-  println(market)
-  (1 to 10) foreach { i =>
-    println("=======: " + i)
-    val x = market.sell(6000)
-    println(x)
-    println(market.buy(x))
-  }
-  println(market)
+
+  val manager = new TockerChangerManager()
+  manager.addBancorToken(BancorToken("BTC", 1000000000000L, 500000L))
+  manager.addBancorToken(BancorToken("LTC", 1000000000000L, 50000000L))
+
+  println(manager)
+  println(manager.getTokenChanger("BTC", "LTC").quoteSellPrice(1000))
+  println(manager)
 }
