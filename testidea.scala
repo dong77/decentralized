a1f6ef74dd6a005a907ff398e0174308edb581ca
wangdong <wangdong@zhongan.io>
2017-06-13 22:40:12 +0800

    do not use double to make the calculation

--- testidea.scala
+++ testidea.scala
@@ -1,27 +1,38 @@
 package io.dong.makermaker
 
-case class Order(amountA: Long /*out*/ , amountB: Long /*in*/ ) {
-  lazy val rate = amountA.toDouble / amountB
+object Const {
+  val RATE_AMPLIFIER = 10000000
 }
 
-case class OrderX(order: Order, discount: Double, feePercentage: Double = 0.1) {
+case class Order(amountA: Long /*out*/ , amountB: Long /*in*/ ) {
+  lazy val rate = amountA * Const.RATE_AMPLIFIER / amountB
+}
+
+case class OrderX(order: Order, discount: Long, feePercentage: Double = 0.1) {
   var amountA: Long = order.amountA
   var amountB: Long = order.amountB
 
-  lazy val actualRate = order.rate * (1 - discount)
+  lazy val actualRate = {
 
-  def savings = (amountB * order.rate).toLong - amountA
+    val ar = order.rate * (Const.RATE_AMPLIFIER - discount) / Const.RATE_AMPLIFIER
+    println("rate: " + order.rate + " actual rate is " + ar)
+    ar
+  }
+
+  def savings = (amountB * order.rate - amountA * Const.RATE_AMPLIFIER) / Const.RATE_AMPLIFIER
+
   def fee = {
     val f = Math.floor(savings * feePercentage).toLong
     Math.min(order.amountA - amountA, f)
+    // 0
   }
   def remaningAmountA = order.amountA - amountA - fee
-  def remaningAmountB = Math.floor(remaningAmountA / order.rate).toLong
+  def remaningAmountB = remaningAmountA * Const.RATE_AMPLIFIER / order.rate
 
   override def toString() = s"$order [ $amountA / $actualRate => ${amountB} $fee(fee))]"
 
   def updatedOrder = {
-    println(s"paied $amountA and fee $fee")
+    println(s"paid $amountA and fee $fee")
     Order(remaningAmountA, remaningAmountB)
   }
 }
@@ -29,7 +40,9 @@ case class OrderX(order: Order, discount: Double, feePercentage: Double = 0.1) {
 object Main extends App {
   val orders = Seq(Order(10000 * 1000, 10 * 1000), Order(9 * 1000, 110 * 1000), Order(100 * 1000, 8000 * 1000))
 
-  val discount = 1 - Math.pow(1 / orders.map(_.rate).reduce(_ * _), 1.0 / orders.size)
+  val discount = ((1 - Math.pow(1 / orders.map(_.rate.toDouble / Const.RATE_AMPLIFIER).reduce(_ * _), 1.0 / orders.size)) * Const.RATE_AMPLIFIER).toLong
+
+  println("discount: " + discount)
   val orders2 = orders.map { order => OrderX(order, discount) }
 
   val size = orders.size
@@ -41,7 +54,7 @@ object Main extends App {
     val o1 = orders2(i % size)
     val o2 = orders2((i + 1) % size)
 
-    o1.amountB = Math.floor(amountA / o1.actualRate).toLong
+    o1.amountB = amountA * Const.RATE_AMPLIFIER / o1.actualRate
 
     if (o1.amountB > o2.amountA) {
       bottleNetIndex = i + 1
